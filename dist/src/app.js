aa22f19696eab1114242ca537ea67f7256f1b85a
BrightkyEfoo <brightefoo@gmail.com>
2023-12-02 14:17:22 +0100

    release 1.3.2 with some gorgeous texts

--- dist/src/app.js
+++ dist/src/app.js
@@ -17,20 +17,56 @@ const shelljs_1 = __importDefault(require("shelljs"));
 const rimraf_1 = require("rimraf");
 const fs_1 = require("fs");
 const path_1 = __importDefault(require("path"));
+const process_1 = require("process");
+const colors_1 = __importDefault(require("colors"));
+const loading_spinner_1 = __importDefault(require("loading-spinner"));
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     const root = process.argv[2] || ".";
     const rootPath = path_1.default.resolve(root);
     const myPath = path_1.default.resolve(process.argv[1]);
     let resStr;
-    (0, rimraf_1.rimrafSync)(root);
+    // try remove directory
+    try {
+        startSpinner();
+        if (root === ".") {
+            console.log(colors_1.default.blue("Delete all files"));
+            (0, rimraf_1.rimrafSync)("./*");
+        }
+        else {
+            console.log(colors_1.default.blue("Delete folder " + root));
+            (0, rimraf_1.rimrafSync)(root);
+        }
+    }
+    catch (error) {
+        console.log(colors_1.default.red("error"), error);
+        (0, process_1.exit)(1);
+    }
+    finally {
+        stopSpinner();
+    }
+    console.log(colors_1.default.green("Delete succeeded"));
     shelljs_1.default.mkdir(root);
     shelljs_1.default.cd(root);
+    startSpinner();
+    console.log(colors_1.default.blue("initialize git"));
     shelljs_1.default.exec("git init");
+    console.log(colors_1.default.green("success"));
+    stopSpinner();
+    startSpinner();
+    console.log(colors_1.default.blue("initialize npm project"));
     shelljs_1.default.exec("npm init -y");
+    console.log(colors_1.default.green("success"));
+    stopSpinner();
     //   copy files
+    startSpinner();
+    console.log(colors_1.default.blue("Copy project files"));
     shelljs_1.default.cp("-r", path_1.default.join(myPath, "..", "..", "files", "*"), ".");
     shelljs_1.default.cp("-r", path_1.default.join(myPath, "..", "..", "files", ".*"), ".");
+    console.log(colors_1.default.blue("success"));
+    stopSpinner();
     //   read package.json file
+    startSpinner();
+    console.log(colors_1.default.blue("add dependencies to package.json"));
     const packageJson = JSON.parse((0, fs_1.readFileSync)(path_1.default.join(rootPath, "package.json"), "utf-8"));
     packageJson.description =
         "Created with create-node-express-ts by BrightkyEfoo";
@@ -61,11 +97,17 @@ const main = () => __awaiter(void 0, void 0, void 0, function* () {
         zod: "^3.22.4",
     };
     (0, fs_1.writeFileSync)(path_1.default.join(rootPath, "package.json"), JSON.stringify(packageJson, null, 2));
+    console.log(colors_1.default.green("success"));
+    stopSpinner();
     //   shell.exec("npm i");
     //   shell.exec("npm run dev");
     //   Start spinner
     // shell.cd(path.resolve(root));
+    startSpinner();
+    console.log(colors_1.default.blue("installing project dependencies"));
     resStr = shelljs_1.default.exec("npm i");
+    console.log(colors_1.default.green("success"));
+    stopSpinner();
     //   stop spinner
     shelljs_1.default.echo(`
   Success. 
@@ -73,3 +115,15 @@ const main = () => __awaiter(void 0, void 0, void 0, function* () {
   `);
 });
 main();
+function startSpinner() {
+    loading_spinner_1.default.start(100, // Interval (in ms) between each spinner sequence element
+    {
+        clearChar: true, // Clear the spinner when stop() is called
+        clearLine: true, // Clear the entire line when stop() is called
+        doNotBlock: false, // Does not prevent the process from exiting
+        hideCursor: true, // Hide the cursor until stop() is called
+    });
+}
+function stopSpinner() {
+    loading_spinner_1.default.stop();
+}
--- src/app.ts
+++ src/app.ts
@@ -2,22 +2,62 @@
 import shell from "shelljs";
 import { rimrafSync } from "rimraf";
 import { readFileSync, readSync, writeFileSync } from "fs";
+import { createInterface } from "readline";
 import path from "path";
+import { exit } from "process";
+import colors from "colors";
+import loadingSpinner, { LoadingSpinner } from "loading-spinner";
 
 const main = async () => {
   const root = process.argv[2] || ".";
   const rootPath = path.resolve(root);
   const myPath = path.resolve(process.argv[1]);
   let resStr: string;
-  rimrafSync(root);
+  // try remove directory
+  try {
+    startSpinner();
+    if (root === ".") {
+      console.log(colors.blue("Delete all files"));
+      rimrafSync("./*");
+    } else {
+      console.log(colors.blue("Delete folder " + root));
+      rimrafSync(root);
+    }
+  } catch (error) {
+    console.log(colors.red("error"), error);
+    exit(1);
+  } finally {
+    stopSpinner();
+  }
+  console.log(colors.green("Delete succeeded"));
+
   shell.mkdir(root);
   shell.cd(root);
+
+
+  startSpinner();
+  console.log(colors.blue("initialize git"));
   shell.exec("git init");
+  console.log(colors.green("success"));
+  stopSpinner();
+
+  startSpinner();
+  console.log(colors.blue("initialize npm project"));
   shell.exec("npm init -y");
+  console.log(colors.green("success"));
+  stopSpinner();
+
   //   copy files
+  startSpinner();
+  console.log(colors.blue("Copy project files"));
   shell.cp("-r", path.join(myPath, "..", "..", "files", "*"), ".");
   shell.cp("-r", path.join(myPath, "..", "..", "files", ".*"), ".");
+  console.log(colors.blue("success"));
+  stopSpinner();
+
   //   read package.json file
+  startSpinner();
+  console.log(colors.blue("add dependencies to package.json"));
   const packageJson = JSON.parse(
     readFileSync(path.join(rootPath, "package.json"), "utf-8")
   );
@@ -54,11 +94,19 @@ const main = async () => {
     path.join(rootPath, "package.json"),
     JSON.stringify(packageJson, null, 2)
   );
+  console.log(colors.green("success"));
+  stopSpinner();
+
   //   shell.exec("npm i");
   //   shell.exec("npm run dev");
   //   Start spinner
   // shell.cd(path.resolve(root));
+  startSpinner();
+  console.log(colors.blue("installing project dependencies"));
   resStr = shell.exec("npm i");
+  console.log(colors.green("success"));
+  stopSpinner();
+  
   //   stop spinner
 
   shell.echo(`
@@ -68,3 +116,21 @@ const main = async () => {
 };
 
 main();
+
+function startSpinner() {
+  (loadingSpinner as LoadingSpinner).start(
+    100, // Interval (in ms) between each spinner sequence element
+    {
+      clearChar: true, // Clear the spinner when stop() is called
+      clearLine: true, // Clear the entire line when stop() is called
+      doNotBlock: false, // Does not prevent the process from exiting
+      hideCursor: true, // Hide the cursor until stop() is called
+    }
+    ,
+    
+  );
+}
+
+function stopSpinner() {
+  (loadingSpinner as LoadingSpinner).stop();
+}
